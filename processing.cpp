9a91a4dc48c039350250a14ae99c2288ee4403ad
Rohan Paranjpe <rohan@Amulyas-MacBook-Air.local>
2015-08-02 21:08:09 -0700

    Abstracted code to mapping function (currently includes sample encoding the english alphabet)

--- processing.cpp
+++ processing.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <queue>
+#include <map>
 #include "HuffmanBinaryTree.hpp"
 
 class BinaryTreeNodeComparator
@@ -35,23 +36,55 @@ void debug_printLeafCodes(const HBTNode* root) {
     }
 }
 
-int main() {
+// Constructs a Huffman Tree from a map of symbols to ints (frequencies).
+HBTNode* constructTree(const std::map<char, int> symbolFrequencyMap) {
     std::priority_queue<HBTNode*, std::vector<HBTNode*>, BinaryTreeNodeComparator> pq;
-    char symbols[6] = {'a','b','c','d','e','f'};
-    int frequencies[6] = {20, 15, 10, 7, 45, 5};
-    for(int i = 0; i < 6; i++) {
-        pq.push(new HBTNode(frequencies[i], symbols[i]));
+    for (auto iter = symbolFrequencyMap.begin(); iter != symbolFrequencyMap.end(); iter++) {
+        pq.push(new HBTNode(iter -> second, iter -> first));
     }
     while(pq.size() > 1) {
-        HBTNode* left = pq.top();
-        pq.pop();
         HBTNode* right = pq.top();
+        pq.pop();
+        HBTNode* left = pq.top();
         pq.pop();
         HBTNode* internal = new HBTNode(left, right);
         left -> setParent(internal);
         right -> setParent(internal);
         pq.push(internal);
     }
-    debug_printLeafCodes(pq.top());
+    return pq.top();
+}
+
+int main() {
+    std::map<char, int> symbolMap;
+
+    symbolMap.insert(std::pair<char, int>('a', 8167));
+    symbolMap.insert(std::pair<char, int>('b', 1492));
+    symbolMap.insert(std::pair<char, int>('c', 2782));
+    symbolMap.insert(std::pair<char, int>('d', 4253));
+    symbolMap.insert(std::pair<char, int>('e', 12702));
+    symbolMap.insert(std::pair<char, int>('f', 2228));
+    symbolMap.insert(std::pair<char, int>('g', 2015));
+    symbolMap.insert(std::pair<char, int>('h', 6094));
+    symbolMap.insert(std::pair<char, int>('i', 6966));
+    symbolMap.insert(std::pair<char, int>('j', 153));
+    symbolMap.insert(std::pair<char, int>('k', 772));
+    symbolMap.insert(std::pair<char, int>('l', 4025));
+    symbolMap.insert(std::pair<char, int>('m', 2406));
+    symbolMap.insert(std::pair<char, int>('n', 6749));
+    symbolMap.insert(std::pair<char, int>('o', 7507));
+    symbolMap.insert(std::pair<char, int>('p', 1929));
+    symbolMap.insert(std::pair<char, int>('q', 95));
+    symbolMap.insert(std::pair<char, int>('r', 5987));
+    symbolMap.insert(std::pair<char, int>('s', 6327));
+    symbolMap.insert(std::pair<char, int>('t', 9056));
+    symbolMap.insert(std::pair<char, int>('u', 2758));
+    symbolMap.insert(std::pair<char, int>('v', 978));
+    symbolMap.insert(std::pair<char, int>('w', 2361));
+    symbolMap.insert(std::pair<char, int>('x', 150));
+    symbolMap.insert(std::pair<char, int>('y', 1974));
+    symbolMap.insert(std::pair<char, int>('z', 74));
+
+    debug_printLeafCodes(constructTree(symbolMap));
 }
 
