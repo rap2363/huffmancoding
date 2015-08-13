ad19f06290a11d30e94db987ddea8c2063eab09d
Rohan Paranjpe <rap2363@gmail.com>
2015-08-13 00:04:02 -0700

    Remove unused code from BinaryEncoder
    -Make BinaryEncoder leaner (remove bits field and accessor/setter methods)
    -Remove old compression/decompression methods

--- datastructures/BinaryEncoder.cpp
+++ datastructures/BinaryEncoder.cpp
@@ -6,50 +6,6 @@
 
 const unsigned NUM_CHAR_BITS = 8;
 
-// Read in ASCII characters from a text file encoded with a header byte indicating how many bits to ignore in the last byte.
-void BinaryEncoder::streamInBinaryFile(std::ifstream &ifs) {
-    if (!ifs) {
-        std::cerr << "Could not open file to stream bits from!" << std::endl;
-    }
-    else {
-        char c;
-        unsigned header_byte;
-        bool isHeader = true;
-        while (ifs.get(c)) {
-            if (isHeader) {
-                header_byte = c;
-                isHeader = false;
-            }
-            else {
-                for (int i = 1; i <= NUM_CHAR_BITS; i++) {
-                    this -> addBit((c >> (NUM_CHAR_BITS - i))  %2);
-                }
-            }
-        }
-        while (header_byte) {
-            this -> removeBit();
-            header_byte--;
-        }
-    }
-}
-
-// Streams out to a character file from the bits in the m_bits vector, using a Huffman Binary Tree.
-void BinaryEncoder::streamOutCharacterFile(std::ofstream &ofs, HBTNode* root) {
-    if (!ofs) {
-        std::cerr << "Could not open file to stream characters to!" << std::endl;
-    }
-    else {
-        HBTNode* currentNode = root;
-        for (auto iter = m_bits.begin(); iter != m_bits.end(); ++iter) {
-            currentNode = *iter ? currentNode -> getRight() : currentNode -> getLeft();
-            if (currentNode -> isLeaf()) {
-                ofs.put(currentNode -> getSymbol());
-                currentNode = root;
-            }
-        }
-    }
-}
-
 void BinaryEncoder::addBinarySequenceToCharacterBuffer(char* &char_buffer, const std::vector<bool> &bits, int &buffer_index, int &bit_index) {
     unsigned char last_buffer_char = bit_index % NUM_CHAR_BITS == 0 ? 0 : char_buffer[buffer_index];
     unsigned char char_to_write = last_buffer_char;
@@ -135,34 +91,4 @@ void BinaryEncoder::decompressFromBinary(std::ifstream &bin_fs, std::ofstream &c
         }
     }
     char_fs.write(&char_buffer[0], sizeof(char)*char_buffer.size());
-}
-
-void BinaryEncoder::addBit(bool bit) {
-    this -> m_bits.push_back(bit);
-}
-
-void BinaryEncoder::removeBit() {
-    this -> m_bits.pop_back();
-}
-
-// DEBUG METHOD DELETE ME
-void BinaryEncoder::debugPrint() {
-    for (int i =0; i < this -> m_bits.size(); i++) {
-        std::cout << this -> m_bits[i];
-    }
-    std::cout << std::endl;
-}
-
-void BinaryEncoder::addBits(std::vector<bool> bits) {
-    for (int i = 0, bit_length=bits.size(); i < bit_length; i++) {
-        this -> addBit(bits[i]);
-    }
-}
-
-void BinaryEncoder::clearBits() {
-    this -> m_bits.clear();
-}
-
-unsigned BinaryEncoder::getStreamSize() {
-    return this -> m_bits.size();
 }
--- datastructures/BinaryEncoder.hpp
+++ datastructures/BinaryEncoder.hpp
@@ -10,26 +10,12 @@
 #include "HuffmanBinaryTree.hpp"
 
 class BinaryEncoder {
-    std::deque<bool> m_bits;
     void addBinarySequenceToCharacterBuffer(char* &, const std::vector<bool> &, int &, int &);
 public:
     BinaryEncoder() {}
 
-    void debugPrint(); // DEBUG METHOD DELETE ME
-    void streamInBinaryFile(std::ifstream &);
-    void streamOutCharacterFile(std::ofstream &, HBTNode*);
-
     void compressToBinary(std::ifstream &, const int, std::ofstream &, const std::map<char, const std::vector<bool> > &);
     void decompressFromBinary(std::ifstream &, std::ofstream &, const HBTNode* &);
-
-    void addBit(bool);
-    void addBits(std::vector<bool>);
-    void removeBit();
-
-    void clearBits();
-
-    unsigned getStreamSize();
-
 };
 
 #endif
